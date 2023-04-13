1b26d73d485f7606fc2887e68564db495e7c4f57
Tororo98 <sebastiantofra98@javerianacali.edu.co>
2023-04-13 02:17:20 -0500

    Class example

--- inputVariables.py
+++ inputVariables.py
@@ -1,2 +1,21 @@
 class soldier:
-    pass
+    # constructor de la clase
+    def __init__(self, rank) -> None:
+        self.rank = rank
+        self.health = rank*1000
+        self.strength = rank*100
+        self.speed = rank*0.7
+        self.flyes = False
+    
+    def take_wings(self):
+        self.flyes = True
+
+    def take_damage(self, dmg):
+        self.health -= dmg
+
+    def hit(self) -> int:
+        return self.strength
+
+tropa1 = soldier(3)
+tropa1.take_wings()
+print(tropa1.flyes)
